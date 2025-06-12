79c6b69320ec96ec36d638ead6af313d5c855e79
loris4444 <bevilacqualoris4444@gmail.com>
2025-06-12 17:22:00 +0200

    Aggiunto stile e icone prime ng

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,11 +3,20 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 import { provideZoneChangeDetection } from '@angular/core';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { providePrimeNG } from 'primeng/config';
+import Nora from '@primeng/themes/nora';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient()
+    provideHttpClient(),
+    provideAnimationsAsync(),
+        providePrimeNG({
+            theme: {
+                preset: Nora
+            }
+        })
   ]
 };
